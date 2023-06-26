80132b50c132ce32b31ed55628e9d28c797f4238
Mobashara215 <mubasheramobashara121@gmail.com>
2023-06-26 19:32:32 +0600

    new

---
+++ week 3/day 5/(a+b).cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+
+    int test;
+    cin >> test;
+
+    while (test--){
+        int a,b;
+        cin>>a>>b;
+        int final=a+b;
+        cout<<final<<endl;
+
+    }
+    return 0;
+}
