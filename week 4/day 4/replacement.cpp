c04f5df0d67b22e8efd17c9ae2656bbca1e7cffc
Mobashara215 <mubasheramobashara121@gmail.com>
2023-06-30 21:28:18 +0600

    new

---
+++ week 4/day 4/replacement.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;cin>>n;
+        int a[n];
+        for(int i=0;i<n;i++){cin>>a[i]; }
+
+        string s;cin>>s;
+
+        map<int,set<char>>k;
+
+        for(int i=0;i<n;i++)
+        {
+            k[a[i]].insert(s[i]);
+        }
+        bool flag=true;
+        for(auto it: k){
+            if(it.second.size()>1){
+                flag=false;
+                break;
+
+            }
+        }
+        if(flag==true){cout<<"YES"<<endl;}
+        else{cout<<"NO"<<endl;}
+    }
+    return 0;
+}
