8a6b60aef50775b4dc6a1a3645d3467577e3a116
Mobashara215 <mubasheramobashara121@gmail.com>
2023-06-30 21:46:35 +0600

    new

---
+++ week 4/day 5/decode_string.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+    
+
+    int test;
+       cin>>test;
+    while (test--)
+    {
+        
+        int n; 
+         cin>>n;
+        string a; 
+          cin>>a;
+        string ans="";
+        long long val=0;
+        for(int i=n-1;i>=0;)
+        {
+            if(a[i]=='0')
+            {
+                val=(a[i-2]-48)*10+a[i-1]-48;
+                i-=3;
+            }
+            else
+            {
+                val=a[i]-48;
+                i--;
+            }
+            ans=ans+char(val+97-1);
+        }
+        reverse(ans.begin(),ans.end());
+        cout<<ans<<endl;
+    }
+    
+    return 0;
+}
