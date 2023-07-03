1f25ff2dda6ae7aff5e1e583c2fc69ee1929d031
Mobashara215 <mubasheramobashara121@gmail.com>
2023-07-03 21:08:52 +0600

    new

---
+++ week 4/day 8/remove_prefix.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+ 
+int main()
+{
+   
+    int test;  
+     cin>>test;
+    for(;test--;)
+    {
+        int n;  cin>>n;
+        set<int>st;
+        int visited=0;
+        vector<int>a(n);
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+        }
+        for(int i=n-1;i>=0;i--)
+        {
+            st.insert(a[i]);
+            visited++;
+            if(visited>st.size())
+            {
+                break;
+            }
+        }
+        cout<<(n-st.size());
+        cout<<endl;
+    }
+    return 0;
+}
