18964e10699a478aab92c5a97a30c1e6bfd34aaa
Mobashara215 <mubasheramobashara121@gmail.com>
2023-07-04 22:52:50 +0600

    new

---
+++ week 4/day 9/train_and_quries.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+ 
+void solve()
+{
+    int n,k;
+    cin>>n>>k;
+    map<int,vector<int>>mp;
+    for(int i=1;i<=n;i++){
+        int j;  cin>>j;
+        mp[j].push_back(i);
+    }
+    for(int i=0;i<k;i++)
+    {
+        int a,b;    cin>>a>>b;
+        if(!mp.count(a) || !mp.count(b))
+        {
+            cout<<"NO"<<endl;
+            continue;
+        }
+        int idx=mp[a][0];
+        if(upper_bound(mp[b].begin(),mp[b].end(),idx)==mp[b].end()){
+            cout<<"NO"<<endl;
+        }else{
+            cout<<"YES"<<endl;
+        }
+    }
+}
+ 
+int main()
+{
+   
+ 
+    int t;
+    cin>>t;
+    while (t--)
+    {
+    
+        solve();
+    }
+    
+    return 0;
+}
