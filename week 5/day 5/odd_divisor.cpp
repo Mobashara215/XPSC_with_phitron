2dd5e36a4249b5c15acad8bfb58922b7c7fcedc8
Mobashara215 <mubasheramobashara121@gmail.com>
2023-07-12 21:31:22 +0600

    new

---
+++ week 5/day 5/odd_divisor.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        long long m,i;
+        cin>>m;
+        int flag=0;
+ 
+        if(m&(m-1)){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+    }
+    return 0;
+}
