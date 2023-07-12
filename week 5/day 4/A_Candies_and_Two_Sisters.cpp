d2c0ac22c02faa4af2221cd5685b591ed240ad44
Mobashara215 <mubasheramobashara121@gmail.com>
2023-07-12 19:34:54 +0600

    new

---
+++ week 5/day 4/A_Candies_and_Two_Sisters.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int test;
+    cin>>test;
+    while(test--)
+    {
+        int n;
+        cin>>n;
+        int division=round((n-1)/2);
+        cout<<division<<endl;
+    }
+    return 0;
+}
