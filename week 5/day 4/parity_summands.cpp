d64526a746d2b3dab6c3bc11024162543e00c949
Mobashara215 <mubasheramobashara121@gmail.com>
2023-07-12 20:13:58 +0600

    new

---
+++ week 5/day 4/parity_summands.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    for(;t--;)
+    {
+        int n,k;
+        cin>>n>>k;
+        
+        
+        int odd=n-(k-1);
+        if(odd>0 && odd%2 == 1)
+        {
+            cout<<"YES"<<"\n";
+            for(int i=0;i<k-1;i++)
+            {
+                cout<<"1"<<" ";
+            }
+            cout<<odd<<"\n";
+            continue;
+        }
+        
+        
+        int eve=n-2*(k-1);
+        if(eve>0 && eve%2 == 0)
+        {
+            cout<<"YES"<<"\n";
+            for(int i=0;i<k-1;i++)
+            {
+                cout<<"2"<<" ";
+            }
+            cout<<eve<<"\n";
+            continue;
+        }
+        cout<<"NO"<<"\n";
+    }
+    return 0;
