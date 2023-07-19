3387691169a83a2564a59966d91cd1b0cc21dc65
Mobashara215 <mubasheramobashara121@gmail.com>
2023-07-19 10:52:40 +0600

    new

---
+++ week 6/day 6/four_ticket.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+    int x;
+    cin>>x;
+    int m= (x*4);
+    if(m<=1000)
+    {
+        cout<<"YES"<<endl;
+    }
+    else
+    {
+        cout<<"NO"<<endl;
+    }
+}
+}
