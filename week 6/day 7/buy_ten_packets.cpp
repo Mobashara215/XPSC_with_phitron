8ee38ed7eab0380865a4b59ddd77aba4ee93ac47
Mobashara215 <mubasheramobashara121@gmail.com>
2023-07-20 21:53:47 +0600

    new

---
+++ week 6/day 7/buy_ten_packets.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+int main()
+
+{
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+    int x,y;   
+    cin>>x>>y;
+    cout<<((2*y)+(x*1))<<endl;
+}
+}
