d1959db0569a819f47a0b64004bf920d25f0a640
Mobashara215 <mubasheramobashara121@gmail.com>
2023-07-21 23:11:33 +0600

    new

---
+++ week 7/we_need_the_zero.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;  
+    cin>>t;
+    while(t--){
+        int n; 
+        cin>>n;
+        vector<int>a(n);
+        for(int i=0;i<n;i++)   
+        cin>>a[i];
+        int m=0;
+        for(int i=0;i<n;i++){
+            m^=a[i];
+        }
+        for(int i=0;i<n;i++){
+            a[i]^=m;
+        }
+        int final=0;
+        for(int i=0;i<n;i++){
+            final^=a[i];
+        }
+        if(final==0){
+            cout<<m<<endl;
+        }else{
+            cout<<"-1"<<endl;
+        }
+    }
+ 
+    return 0;
+}
