1298e397dcd93ace581f73e3a53a1b4b0f38e91e
Mobashara215 <mubasheramobashara121@gmail.com>
2023-07-21 23:03:53 +0600

    new

---
+++ week 7/a_xor_wice.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t; 
+     cin>>t;
+    while(t--){
+        int A,B;
+        cin>>A>>B;
+        int final=A^B;
+        cout<<final<<endl;
+    }
+
+    return 0;
+}
