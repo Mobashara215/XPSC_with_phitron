5e15490a6e05907dc3e4635d21a0f153ae4a262d
Mobashara215 <mubasheramobashara121@gmail.com>
2023-07-22 22:25:28 +0600

    new

---
+++ week 7/day 2/divisible_array.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    
+    int t;
+    cin>>t;
+    while(t--){
+        int p;
+        cin>>p;
+        for(int i=1;i<=p;i++){
+            cout<<(i*2)<<" ";
+        }
+        cout<<endl;
+    }
+ 
+    return 0;
+}
