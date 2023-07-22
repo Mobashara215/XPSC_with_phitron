60d3f1829dbd6733ff59f35a125390df198622e6
Mobashara215 <mubasheramobashara121@gmail.com>
2023-07-22 21:44:15 +0600

    new

---
+++ week 7/day1/bitwise_exclusive_or.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll; 
+
+int main()
+{
+    int A,B;
+    cin>>A>>B;
+    int final=A^B;
+    cout<<final<<endl;
+    return 0;
+}
