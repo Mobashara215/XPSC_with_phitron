edf1dcde847bf372eaa4478e8413e785f2b8596f
Mobashara215 <mubasheramobashara121@gmail.com>
2023-07-23 21:24:47 +0600

    new

---
+++ week 7/day 3/good_tern.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+    int x,y;
+    cin>>x>>y;
+    int ans=(x+y);
+    if(ans>6)
+    {
+        cout<<"YES"<<endl;
+    }
+    else
+    {
+        cout<<"NO"<<endl;
+    }
+}
+}
