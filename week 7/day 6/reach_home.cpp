82f42dc20cb14d3ef8c0d8e29572d47db79af46b
Mobashara215 <mubasheramobashara121@gmail.com>
2023-07-26 19:34:08 +0600

    new

---
+++ week 7/day 6/reach_home.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,m;
+        cin>>n>>m;
+        if((n*5)>=m)
+        {
+            cout<<"YES"<<endl;
+        } 
+        else{
+        cout<<"NO"<<endl;
+    }
+    }
+}
