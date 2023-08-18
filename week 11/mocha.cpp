df1e1fd962eed239d7bbb42b294387cd08519cab
Mobashara215 <mubasheramobashara121@gmail.com>
2023-08-18 23:21:33 +0600

    new

---
+++ week 11/mocha.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;  cin>>t;
+    while(t--){
+        int n;  cin>>n;
+        vector<int>a(n);
+        for(int i=0;i<n;i++)    cin>>a[i];
+        int temp=a[0];
+        for(int i=1;i<n;i++){
+            temp=(temp & a[i]);
+        }
+        cout<<temp<<endl;
+    }
+ 
+    return 0;
+}
