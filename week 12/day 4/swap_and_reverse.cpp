893095b932ab01cb83383c7bc267ed67836c8914
Mobashara215 <mubasheramobashara121@gmail.com>
2023-08-28 23:01:17 +0600

    new

---
+++ week 12/day 4/swap_and_reverse.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+#define in_range(a,b)    for(int i=a;i<b;i++)
+#define read(x) int x; cin >> x
+int main()
+{
+
+    int t;  cin>>t;
+    while(t--){
+        int n,k;    cin>>n>>k;
+        string s;   cin>>s;
+        vector<char>od,ev;
+        for(int i=0;i<n;i++){
+            if(i%2!=0){
+                od.push_back(s[i]);
+            }
+            else{
+                ev.push_back(s[i]);
+            }
+        }
+        string final="";
+        sort(ev.begin(),ev.end());
+        sort(od.begin(),od.end());
+        for(int i=0,j=0;i<ev.size() || j<od.size();i++,j++){
+            if(i<ev.size()){
+                final+=ev[i];
+            }
+            if(j<od.size()){
+                final+=od[i];
+            }
+        }
+        if(k%2==0){
+            sort(s.begin(),s.end());
+            cout<<s<<"\n";
+            continue;
+        }
+        cout<<final<<endl;
+    }
+ 
+    return 0;
+}
